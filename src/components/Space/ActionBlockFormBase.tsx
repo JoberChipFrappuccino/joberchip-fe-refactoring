3b7dbd8c43ecab903ff0a749804a361aa7610d02
peacepiece7 <peacepiece7@naver.com>
2023-09-11 16:17:35 +0900

    feat : drawer block type 이동 되도록 수정 #5

--- src/components/Space/ActionBlockFormBase.tsx
+++ src/components/Space/ActionBlockFormBase.tsx
@@ -1,5 +1,7 @@
+import { type BlockType } from '@/models/space'
 import { useDrawerFormType } from '@/store/formMode'
 import type { ReactNode } from 'react'
+import styles from './ActionBlockFormBase.module.scss'
 
 // todo block type을 알아야하는지 논의가 필요합니당.
 interface Props {
@@ -7,12 +9,55 @@ interface Props {
 }
 
 export function ActionBlockFormBase({ children }: Props) {
-  const { blockType } = useDrawerFormType()
+  const { blockType, setBlockType } = useDrawerFormType()
+
+  type FormType = {
+    title: string
+    type: BlockType
+    icon: string | ReactNode
+  }
+  const formTypes: FormType[] = [
+    {
+      type: 'link',
+      title: '링크',
+      icon: 'icon'
+    },
+    {
+      type: 'image',
+      title: '사진',
+      icon: 'icon'
+    },
+    {
+      type: 'googleMap',
+      title: '지도',
+      icon: 'icon'
+    },
+    {
+      type: 'text',
+      title: '텍스트',
+      icon: 'icon'
+    }
+  ]
   return (
     <div>
-      <h1>{blockType}</h1>
-      {/* todo switch Image case */}
-      <p>이 부분은 이미지가 클릭 & 활성화 되는 영역입니다.</p>
+      <h1>{blockType} 추가하기</h1>
+      <div className={styles.navigation}>
+        {formTypes.map((form) => {
+          return (
+            <div key={form.type}>
+              <button
+                className={form.type === blockType ? styles.active : styles.inactive}
+                onClick={() => {
+                  setBlockType(form.type)
+                }}
+              >
+                <div>{form.icon}</div>
+                <div>{form.title}</div>
+              </button>
+            </div>
+          )
+        })}
+      </div>
       <div>{children}</div>
     </div>
   )
