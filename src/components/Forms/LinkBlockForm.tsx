e84d35e0bdb09490eed7df2265977bfb6eb7bfc5
DICEPT <dicepted@gmail.com>
2023-09-13 21:34:09 +0900

    feat : 링크 제목 및 주소 문자열 입력 후 버튼 활성화/비활성화 기능 #20

--- src/components/Forms/LinkBlockForm.tsx
+++ src/components/Forms/LinkBlockForm.tsx
@@ -1,9 +1,11 @@
 import { useState, type ChangeEvent, type FormEvent } from 'react'
+import FormButton from '../Ui/Button'
 import styles from './LinkBlockForm.module.scss'
 
 export default function LinkBlockForm() {
   const [link, setLink] = useState<string>('')
   const [title, setTitle] = useState<string>('')
+  const isButtonDisabled = !link || !title
 
   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -16,25 +18,23 @@ export default function LinkBlockForm() {
   }
 
   const onChangeLink = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
     setLink(e.target.value)
   }
 
   const onChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
     setTitle(e.target.value)
   }
 
   return (
     <div className={styles.container}>
-      <form onSubmit={submitHandler}>
-        <h3>URL 링크 주소 제목</h3>
-        <input type="text" value={title} onChange={onChangeTitle} placeholder="링크 제목을 입력해주세요." />
-        <h3>URL 링크 주소 삽입</h3>
-        <input type="text" value={link} onChange={onChangeLink} placeholder="링크 주소를 입력해주세요." />
-        <button className={styles.button} type="submit">
-          링크 추가하기
-        </button>
+      <form className={styles.formBox} onSubmit={submitHandler}>
+        <div>
+          <h3>URL 링크 주소 제목</h3>
+          <input type="text" value={title} onChange={onChangeTitle} placeholder="링크 제목을 입력해주세요." />
+          <h3>URL 링크 주소 삽입</h3>
+          <input type="text" value={link} onChange={onChangeLink} placeholder="링크 주소를 입력해주세요." />
+        </div>
+        <FormButton title={'링크 추가하기'} event={isButtonDisabled} />
       </form>
     </div>
   )
