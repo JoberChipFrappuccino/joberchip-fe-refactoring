c5e4fa88c0c2545edf9d96cb0b9fe4ec4bd01fce
daseul <7581058@gmail.com>
2023-09-18 20:12:19 +0900

    feat : TextEditor 툴바 옵션, 커스텀 스타일 맵 추가 #10

---
+++ src/constants/textEditorOptions.ts
@@ -0,0 +1,206 @@
+// 폰트, 글자 크기, 볼드, 이탤릭 (1)
+// 밑줄, 취소선, 색상, 정렬, 그냥 목록, 숫자목록 (2)
+import styles from '../components/TextEditor/ToolOption.module.scss'
+
+export const TOOL_TYPES = [
+  {
+    tools: [
+      {
+        label: 'font',
+        icon: '',
+        text: '기본서체',
+        action: 'font',
+        type: 'options',
+        option: 'font-options'
+      },
+      {
+        label: 'size',
+        icon: '',
+        text: '14',
+        action: 'size',
+        type: 'options',
+        option: 'size-options'
+      },
+      {
+        label: 'bold',
+        text: '',
+        icon: 'bold.svg',
+        action: 'BOLD',
+        type: 'inline'
+      },
+      {
+        label: 'italic',
+        text: '',
+        icon: 'italic.svg',
+        action: 'ITALIC',
+        type: 'inline'
+      },
+      {
+        label: 'underline',
+        icon: 'underline.svg',
+        action: 'UNDERLINE',
+        type: 'inline',
+        text: ''
+      },
+      {
+        label: 'strikethrough',
+        icon: 'strikethrough.svg',
+        action: 'STRIKETHROUGH',
+        type: 'inline',
+        text: ''
+      }
+    ]
+  },
+  {
+    tools: [
+      {
+        label: 'color',
+        icon: 'color.svg',
+        action: 'color',
+        type: 'options',
+        option: 'color-options',
+        text: ''
+      },
+      {
+        label: 'align',
+        icon: 'align.svg',
+        action: 'align-options',
+        type: 'block',
+        text: ''
+      },
+      {
+        label: 'ordered',
+        icon: 'ordered.svg',
+        action: 'ordered-list-item',
+        type: 'block',
+        text: ''
+      },
+      {
+        label: 'unordered',
+        icon: 'unordered.svg',
+        action: 'unordered-list-item',
+        type: 'block',
+        text: ''
+      }
+    ]
+  }
+]
+
+const start = 14
+const end = 36
+const step = 2
+
+export const SIZE_OPTIONS: number[] = []
+for (let i = start; i <= end; i += step) {
+  SIZE_OPTIONS.push(i)
+}
+
+const sizes: Record<string, { fontSize: string }> = {}
+for (let i = 14; i <= 36; i += 2) {
+  sizes[`size${i}`] = {
+    fontSize: `${i}px`
+  }
+}
+
+export const FONT_OPTIONS = [
+  {
+    font: 'nanum'
+  },
+  {
+    font: 'monospace'
+  },
+  {
+    font: 'pretandard'
+  }
+]
+
+export const TEXT_COLORS_OPTIONS = [
+  { style: 'black', className: styles.black },
+  { style: 'gray', className: styles.gray },
+  { style: 'white', className: styles.white },
+  { style: 'red', className: styles.red },
+  { style: 'orange', className: styles.orange },
+  { style: 'yellow', className: styles.yellow },
+  { style: 'green', className: styles.green },
+  { style: 'blue', className: styles.blue },
+  { style: 'purple', className: styles.purple }
+]
+
+export const TEXT_BGCOLORS_OPTIONS = [
+  { style: 'blackBg', className: styles.blackBg },
+  { style: 'grayBg', className: styles.grayBg },
+  { style: 'whiteBg', className: styles.whiteBg },
+  { style: 'redBg', className: styles.redBg },
+  { style: 'orangeBg', className: styles.orangeBg },
+  { style: 'yellowBg', className: styles.yellowBg },
+  { style: 'greenBg', className: styles.greenBg },
+  { style: 'blueBg', className: styles.blueBg },
+  { style: 'purpleBg', className: styles.purpleBg }
+]
+
+export const StyleMap = {
+  black: {
+    color: '#111111'
+  },
+  gray: {
+    color: '#E2E2E2'
+  },
+  white: {
+    color: '#ffffff'
+  },
+  red: {
+    color: '#FF0000'
+  },
+  orange: {
+    color: '#ff9300'
+  },
+  yellow: {
+    color: '#FFdb30'
+  },
+  green: {
+    color: '#1FA99E'
+  },
+  blue: {
+    color: '#00B3F2'
+  },
+  purple: {
+    color: '#AA1F91'
+  },
+  blackBg: {
+    backgroundColor: '#111111'
+  },
+  grayBg: {
+    backgroundColor: '#E2E2E2'
+  },
+  whiteBg: {
+    backgroundColor: '#ffffff'
+  },
+  redBg: {
+    backgroundColor: '#FF0000'
+  },
+  orangeBg: {
+    backgroundColor: '#ff9300'
+  },
+  yellowBg: {
+    backgroundColor: '#FFdb30'
+  },
+  greenBg: {
+    backgroundColor: '#1FA99E'
+  },
+  blueBg: {
+    backgroundColor: '#00B3F2'
+  },
+  purpleBg: {
+    backgroundColor: '#AA1F91'
+  },
+  font1: {
+    fontFamily: 'monospace'
+  },
+  font2: {
+    fontFamily: 'sans-serif'
+  },
+  font3: {
+    fontFamily: 'serif'
+  },
+  ...sizes
+}
