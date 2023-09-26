434973d17717fc6378e5b7e7d5da947f9bc01c6b
DICEPT <dicepted@gmail.com>
2023-09-26 20:09:17 +0900

    feat : 블록 폼 Props 수정 및 input 초기화 버튼 추가 #62

--- src/components/Forms/LinkBlockForm.tsx
+++ src/components/Forms/LinkBlockForm.tsx
@@ -1,23 +1,22 @@
+import { type BlockWith } from '@/models/space'
 import { useBlockAction } from '@/store/blockAction'
 import { useEffect, useState, type ChangeEvent, type FormEvent } from 'react'
+import { TiDeleteOutline } from 'react-icons/ti'
 import FormButton from '../Ui/Button'
 import styles from './LinkBlockForm.module.scss'
 
 type Props = {
-  block: {
-    text?: string
-    url?: string
-  }
+  block?: BlockWith<'link'>
 }
 
-export default function LinkBlockForm(block: Props) {
+export default function LinkBlockForm({ block }: Props) {
   const [link, setLink] = useState<string>('')
   const [title, setTitle] = useState<string>('')
   const isButtonDisabled = !link || !title
   const { drawerMode } = useBlockAction()
 
-  const titleValue = block.block.text
-  const linkValue = block.block.url
+  const titleValue = block?.text ?? ''
+  const linkValue = block?.url ?? ''
 
   useEffect(() => {
     setTitle(titleValue ?? '')
@@ -52,14 +51,36 @@ export default function LinkBlockForm(block: Props) {
     setTitle(e.target.value)
   }
 
+  const deleteTitle = () => {
+    setTitle('')
+  }
+
+  const deleteLink = () => {
+    setLink('')
+  }
+
   return (
     <div className={styles.container}>
       <form className={styles.formBox} onSubmit={drawerMode === 'create' ? submitAddHandler : submitEditHandler}>
         <div className={styles.forms}>
           <h3>URL 링크 주소 제목</h3>
-          <input type="text" value={title} onChange={onChangeTitle} placeholder="링크 제목을 입력해주세요." />
+          <div className={styles.inputbox}>
+            <input type="text" value={title} onChange={onChangeTitle} placeholder="링크 제목을 입력해주세요." />
+            {title && (
+              <div className={styles.delTitle} onClick={deleteTitle}>
+                <TiDeleteOutline />
+              </div>
+            )}
+          </div>
           <h3 className={styles.formTexts}>URL 링크 주소 삽입</h3>
-          <input type="text" value={link} onChange={onChangeLink} placeholder="링크 주소를 입력해주세요." />
+          <div className={styles.inputbox}>
+            <input type="text" value={link} onChange={onChangeLink} placeholder="링크 주소를 입력해주세요." />
+            {link && (
+              <div className={styles.delLink} onClick={deleteLink}>
+                <TiDeleteOutline />
+              </div>
+            )}
+          </div>
         </div>
         <FormButton title={drawerMode === 'create' ? '링크 추가하기' : '링크 수정하기'} event={isButtonDisabled} />
       </form>
