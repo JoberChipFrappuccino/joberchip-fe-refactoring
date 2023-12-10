4ab76f45fdb09302b08ea86bafd683d5bf7652e2
peacepiece7 <peacepiece7@naver.com>
2023-12-11 02:43:09 +0900

    fix : image, link block form 제출 시 창이 닫히지 않는 문제 수정

--- src/components/SharePage/Forms/LinkBlockForm/EditLinkBlock.tsx
+++ src/components/SharePage/Forms/LinkBlockForm/EditLinkBlock.tsx
@@ -3,16 +3,20 @@ import type { BlockBaseWithBlockFormProps } from '@/components/Common/SwitchCase
 import { LinkBlockForm, type LinkBlockFromInputs } from '@/components/SharePage/Forms/LinkBlockForm/LinkBlockForm'
 import { useEditLinkBlockMutate } from '@/hooks/mutations/linkBlockMutation'
 import { useSharePageQuery } from '@/hooks/queries/useSharePageQuery'
+import { useBlockActionStore } from '@/store/blockAction'
 import styles from './LinkBlockForm.module.scss'
 
 export function EditLinkBlock({ block }: BlockBaseWithBlockFormProps<TLink>) {
   const { pageId } = useSharePageQuery()
   const editLinkMutation = useEditLinkBlockMutate()
+  const { setOpenDrawer } = useBlockActionStore()
+
   const handleSubmit = (data: LinkBlockFromInputs) => {
     const body: EditLinkBlockBody = {
       objectId: block?.objectId ?? '',
       ...data
     }
+    setOpenDrawer(false)
     editLinkMutation.mutate({ pageId, body })
   }
   return (
